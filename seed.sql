a62fddc53d9b2213f7974450a7595b0227827bdd
Tyler Joseph <95454255+tylerjoseph-dev@users.noreply.github.com>
2022-09-29 10:00:54 -0400

    Add files via upload

---
+++ seed.sql
@@ -0,0 +1,73 @@
+INSERT INTO products
+    (product_title, product_description, product_image, product_price, product_likes)
+    VALUES 
+    (
+        'Castles Hill',
+        'An exquisite rendition of an ancient castle with a storm looming in the distance.',
+        'https://i.imgur.com/c8MS4VN.png',
+        119.99,
+        48
+    ),
+    (
+        'Neurological Collapse v2',
+        'An artists depiction of synapses communicating with neighboring neurons.',
+        'https://i.imgur.com/wGvQwiI.png',
+        139.99,
+        110
+    ),
+    (
+        'Dandelion Walk',
+        'A gypsy walks the dandelion path',
+        'https://i.imgur.com/fHDY3bP.png',
+        74.99,
+        45
+    ),
+    (
+        'Robemaster Avzor',
+        'The depiction of the great robemaker, Avzor.',
+        'https://i.imgur.com/Dn76tJn.png',
+        113.99,
+        491
+    ),
+    (
+        'Scholarly Walk v2',
+        'A scholar happens upon a fork in the road, full of indecision.',
+        'https://i.imgur.com/fz3GuYZ.png',
+        109.99,
+        13
+    ),
+    (
+        'Scholarly Walk',
+        'A scholar takes a walk through the university botanical garden.',
+        'https://i.imgur.com/qevFUIm.png',
+        109.99,
+        72
+    ),
+    (
+        'Mists of Time',
+        'A veiled figure watches over the Mists of Time',
+        'https://i.imgur.com/zXB72Sx.png',
+        124.99,
+        520
+    ),
+    (
+        'Neurological Collapse',
+        'An artists depiction of the synapses of the brain.',
+        'https://i.imgur.com/8WHpEVh.png',
+        149.99,
+        135
+    ),
+    (
+        'Robes of Legends',
+        'An artists depiction of the Robes of Legends, created by master robemaker Avzor.',
+        'https://i.imgur.com/rhEyup7.png',
+        107.99,
+        99
+    ),
+    (
+        'Mists of Time v2',
+        'A veiled figure watches as the Mists of Time is corrupted',
+        'https://i.imgur.com/dgd6Ibh.png',
+        99.99,
+        2
+    );
